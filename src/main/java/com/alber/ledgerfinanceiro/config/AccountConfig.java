85a9d27060fd865c7d5f24546a2a962aa4138dfa
Alberto Vilar <albertovilar1@gmail.com>
2026-08-30 17:43:18 -0300

    refactor(config): isolate JPA auditing configuration

--- src/main/java/com/alber/ledgerfinanceiro/config/AccountConfig.java
+++ src/main/java/com/alber/ledgerfinanceiro/config/AccountConfig.java
@@ -9,12 +9,10 @@ import com.alber.ledgerfinanceiro.application.service.TransferService;
 import com.alber.ledgerfinanceiro.application.service.CreateAccountService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.TransactionTemplate;
 
 @Configuration
-@EnableJpaAuditing
 public class AccountConfig {
 
     @Bean
